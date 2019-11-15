8d9c73ff723503d34588d3aa410dab4d35ec1338
gian <gian@lenovo-ubuntu>
2019-11-14 21:23:54 -0300

    prueba git ignore

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=futbol_infantil_v1',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => 'padialls',
+    'password' => '2222',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
